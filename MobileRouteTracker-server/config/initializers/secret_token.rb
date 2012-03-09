dc7461c76db260e1ff1edd5e635fbd42c6de7009
Marc Bellmann <marc.bellmann@mb-entwicklung.de>
2012-03-09 19:12:31 +0100

    init ruby project

---
+++ MobileRouteTracker-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileRouteTrackerServer::Application.config.secret_token = '7943b9095295dd488e6957ffebfdcc35b11c406be9584238a74719dd143f45ab831ab3e9f18b2f0332c061dcf38719954a9d5f234f58259060fca76b7ab78948'
