639582d665a4ecab90c35c5da050aac990ab590e
Marc Bellmann <marc.bellmann@mb-entwicklung.de>
2012-03-10 11:26:58 +0100

    LocationManager run LocationListener

--- MobileRouteTracker-android/src/main/java/de/mbentwicklung/android/mobileroutetracker/MobileRouteTracker.java
+++ MobileRouteTracker-android/src/main/java/de/mbentwicklung/android/mobileroutetracker/MobileRouteTracker.java
@@ -1,16 +1,27 @@
 package de.mbentwicklung.android.mobileroutetracker;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MobileRouteTracker extends Activity {
-	/** Called when the activity is first created. */
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		startService(new Intent(this, TrackerWebservice.class));
+		loadPositon(getApplicationContext());
 	}
+
+	private void loadPositon(final Context context) {
+		final LocationManager locationManager = (LocationManager) context
+				.getSystemService(Context.LOCATION_SERVICE);
+
+		final WebserviceLocationListener l = new WebserviceLocationListener();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 500, l);
+	}
+
 }
