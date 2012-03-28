22057ff6ecd25f1f378cbbad534bcc1483de9245
Marc Bellmann <marc.bellmann@mb-entwicklung.de>
2012-03-28 22:33:37 +0200

    OpenStreetMap optimized

--- MobileRouteTracker-server/app/assets/javascripts/application.js
+++ MobileRouteTracker-server/app/assets/javascripts/application.js
@@ -7,3 +7,62 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+
+ function addMarker(layer, lon, lat, popupContentHTML) {
+ 
+	// Koordinaten in LonLat umwandeln
+	var ll = new OpenLayers.LonLat(lon, lat).transform(
+		new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+		new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
+	);
+
+	// Feature erstellen und konfigurieren (Popup und Marker)
+	var feature = new OpenLayers.Feature(layer, ll);
+	feature.closeBox = false;
+	feature.popupClass = OpenLayers.Class(OpenLayers.Popup.FramedCloud, {minSize: new OpenLayers.Size(200, 100) } );
+	feature.data.popupContentHTML = popupContentHTML;
+	feature.data.overflow = "auto";
+
+	// Marker erstellen
+	var marker = feature.createMarker();
+ 
+	/*
+	 * Handler Funktionen fÃ¼r die Mouse-Events
+	 */
+	// Hover
+	var markerHover = function(evt) {
+		// Wenn das Popup nicht sichtbar ist, dann kann es nicht fest sichtbar sein
+		if (!this.popup.visible()) {
+			this.popup.clicked = false;
+		}
+		if (!this.popup.clicked) {
+			this.popup.show();
+		}
+		OpenLayers.Event.stop(evt);
+	}
+
+	// Hover End
+	var markerHoverEnd = function(evt) {
+		if (!this.popup.clicked) {
+			this.popup.hide();
+		}
+		OpenLayers.Event.stop(evt);
+	}
+
+	// Events auf den Marker registrieren und als Objekt das Feature Ã¼bergeben
+	marker.events.register("mouseover", feature, markerHover);
+	marker.events.register("mouseout", feature, markerHoverEnd);
+
+	// Erstellten Marker der Ebene hinzufÃ¼gen
+	layer.addMarker(marker);
+
+	// Popup erstellen, der Karte hinzufÃ¼gen und anzeigen, falls gewÃ¼nscht
+	map.addPopup(feature.createPopup(feature.closeBox));
+
+	feature.popup.hide();
+	feature.popup.clicked = false;
+
+	return marker;
+}
